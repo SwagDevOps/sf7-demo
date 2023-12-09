f725c9253d28ac62dd39b4aede3d3d08653f1ef3
Dimitri Arrigoni <dimitri@arrigoni.me>
2023-12-09 19:59:47 +0100

    feat(db): introduces database + events entity

---
+++ app/src/Entity/Event.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE)]
+    private ?\DateTimeImmutable $begin_date = null;
+
+    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE)]
+    private ?\DateTimeImmutable $end_date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getBeginDate(): ?\DateTimeImmutable
+    {
+        return $this->begin_date;
+    }
+
+    public function setBeginDate(\DateTimeImmutable $begin_date): static
+    {
+        $this->begin_date = $begin_date;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeImmutable
+    {
+        return $this->end_date;
+    }
+
+    public function setEndDate(\DateTimeImmutable $end_date): static
+    {
+        $this->end_date = $end_date;
+
+        return $this;
+    }
+}
